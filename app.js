9c68b70cadc2b3fd31e917cdcd1f3fe0b5b91732
Vladimir Pavković <vladimirpavk@gmail.com>
2019-03-17 17:59:54 +0100

    +

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ window.onload = function(){
     const reasonInput = document.getElementById('reasonInput');
     const amountInput = document.getElementById('amountInput');
     const expensesList = document.getElementById('expensesList');
+    const totalExpenses = document.getElementById('totalExpenses');
+    const ionAlertCtrl = document.getElementById('alertCtrl');
+
+    let expense = 0;
+
+    const clear = ()=>{        
+        reasonInput.value='';
+        amountInput.value='';
+    }
 
     //Object event listeners
     //HTML Global Event
     clearButton.addEventListener('click', function(){
-        
+        clear();
     });
     //IONIC specific events
     /*clearButton.addEventListener('ionBlur', function(){
@@ -26,13 +35,34 @@ window.onload = function(){
             amountInput.value.trim().length <= 0 ||
             amountInput.value <= 0
         ){
+            ionAlertCtrl.componentOnReady().then(
+                (alertCtrl)=>{
+                    alertCtrl.create({
+                        header: 'Alert',
+                        subHeader: 'Invalid values entered...',
+                        message: 'Please enter valid values',
+                        buttons: ['OK']
+                    }).then(
+                        (alert)=>{
+                            console.log('Present alert');
+                            alert.present();
+                        }
+                    )
+                }
+            )           
+
             return;
-        }
-        //console.log(reasonInput.value, amountInput.value);
+        }       
         const newIonItem = document.createElement('ion-item');
+        //newIonItem.setAttribute('color', 'primary');
         newIonItem.innerHTML = reasonInput.value+' - $'+amountInput.value;
 
         expensesList.appendChild(newIonItem);
+        expense += +amountInput.value;
+        console.log(expense);
+
+        totalExpenses.textContent=expense;
+        clear();
     });
 
 };
